039e1c9cdbebb090ebe22741d57d77fffa877bb0
Maruf Hosen <108074496+marufhosen15@users.noreply.github.com>
2023-09-25 18:07:34 +0600

    nothing

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,14 +1,14 @@
+"use client";
+import React, { useEffect } from "react";
 
-    "use client";
-    import React, { useEffect } from "react";
+const App = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+export default App;
 
-    const App = () => {
-        return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-        );
-    };
-    export default App;
 
-    
+
